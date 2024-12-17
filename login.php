cd3d2e5f38a70cd06581e11f71e78fd6ca85827f
fakasy <140477415+fakasy@users.noreply.github.com>
2024-12-17 10:15:58 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+$username = 'admin';
+$password = '123';
+
+if (isset($_POST['submit'])) {
+    if ($_POST['username'] == $username && $_POST['password'] == $password) {
+        $_SESSION['username'] = $username;
+        echo "Selamat datang, " . $_SESSION['username'];
+        exit();
+    } else {
+        echo "Username atau password salah.";
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username:</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" required>
+        </div>
+        <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password:</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" required>
+        </div>
+        <div class="flex items-center justify-between">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="submit">Login</button>
+        </div>
+    </form>
+</body>
+</html>
+
+
